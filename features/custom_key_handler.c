45b9cfc117bef637b31bc66788274ceaa61e06b7
tomzig16 <tzigmantavicius@gmail.com>
2023-09-04 22:09:00 +0300

    Fix errors

--- features/custom_key_handler.c
+++ features/custom_key_handler.c
@@ -5,7 +5,7 @@
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (detected_host_os()) {
         case OS_MACOS:
-                if(!handle_keyswap_for_macos(keycode, record, )) {
+                if(!handle_keyswap_for_macos(keycode, record)) {
                     // Returns based on the QMK standard - false if system has yet to handle (aka if we handled it)
                     return false;
                 }
